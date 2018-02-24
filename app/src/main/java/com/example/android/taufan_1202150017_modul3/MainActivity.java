99774a78475afb4d473b35decd00dcd9ac417594
TaufanFadhilah <tfi231097@gmail.com>
2018-02-24 22:29:33 +0700

    init commit

---
+++ app/src/main/java/com/example/android/taufan_1202150017_modul3/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.taufan_1202150017_modul3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/com/example/android/taufan_1202150017_modul3/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.android.taufan_1202150017_modul3;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
